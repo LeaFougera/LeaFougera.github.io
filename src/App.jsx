4390582e9dc67979a59695c2feb0af65cbcee7ec
LeaFougera <lea.fougera-lempereur@isen.yncrea.fr>
2026-05-27 18:59:40 +0200

    Add Experience section with timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './pages/Hero'
 import About from './pages/About'
+import Experience from './pages/Experience'
 
 function App() {
   return (
     <main>
       <Hero />
       <About />
+      <Experience />
     </main>
   )
 }
